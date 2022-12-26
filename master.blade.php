5521fa13609cc0a56099b5f5bae61eebc8e067a1
sachiihaxor <106025611+sachiihaxor@users.noreply.github.com>
2022-12-26 12:41:36 +0700

    Create master.blade.php

---
+++ master.blade.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html class="no-js mh-one-sb" lang="en-US" prefix="og: http://ogp.me/ns#">
+
+<head>
+
+    
+<!--GoogleAnalytics-->
+<script type="462354c522f01555b784baeb-text/javascript">
+    (function (i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date(); a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', 'https://google-analytics.com/analytics.js', 'ga');
+
+    ga('create', 'UA-143287096-1', 'auto');
+    ga('send', 'pageview');
+</script>
+
+
+
+<!--GoogleAdsense-->
+<!--<script>-->
+          <!--var delayInMilliseconds = 9000; //1 second-->
+
+<!--          setTimeout(function () {-->
+<!--              if (window.addEventListener)-->
+<!--                  window.addEventListener("load", googleadsense(), false);-->
+<!--              else if (window.attachEvent)-->
+<!--                  window.attachEvent("onload", googleadsense());-->
+<!--              else window.onload = googleadsense();-->
+<!--          }, delayInMilliseconds);-->
+<!--          function googleadsense() {-->
+		    
+	
+<!--              var gcse = document.createElement('script');-->
+<!--              gcse.async = true;-->
+<!--              gcse.setAttribute('data-ad-client', 'ca-pub-5055690937774050');-->
+<!--              gcse.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';-->
+<!--              var s = document.getElementsByTagName('script')[0];-->
+<!--              s.parentNode.insertBefore(gcse, s);-->
+<!--          }-->
+<!--      </script>-->
+
+
+    <!--<meta http-equiv="refresh" content="4839; url={{ Request::url() }}" />-->
+    <meta name="viewport" content="width=device-width">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="yandex-verification" content="183d715885419bb0" />
+    <meta name="msvalidate.01" content="DD71345D70C5D7A0156B95BF25F4F56E" />
+    <meta name="google-site-verification" content="6PDy2L6zxHAITh4AMkZG3oVGIhWqCdpd2hHJN7K2q8A" />
+	<meta name="keywords" content="{{ (!empty($title)) ? $title: Config::get('site.defaultTitle') }},project topics,research topics,research title,research project topics,research paper topics,final year project topics,undergraduates project topics,project topics ideas" />
+	<meta name="description" content="{{ (!empty($meta)) ? $meta : Config::get('site.metaDesc')}}" />
+	<meta name="author" content="{{ucwords(Config::get('site.appname'))}}" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta http-equiv='content-language' content='en-gb'>
+	<link rel="sitemap" href="https://researchwap.com/sitemap.xml" type="application/xml" />
+
+
+	<title>{{ (!empty($title)) ? $title: Config::get('site.defaultTitle') }}</title>
+	
+    <!--<link rel="prev" href="{{ Request::url() }}" />-->
+    <!--<link rel="next" href="{{ Request::url() }}" />-->
+	<link rel="canonical" href="{{ Request::url() }}" />
+	<meta name="robots" content="index, follow" />
+	<meta property="og:locale" content="en_US" />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="{{ (!empty($title)) ? $title: Config::get('site.defaultTitle') }}" />
+	<meta property="og:url" content="{{ Request::url() }}" />
+	<meta property="og:image" content="https://researchwap.com/assets/images/Project%20topics.webp"  />
+	<meta property="og:site_name" content="{{ Config::get('site.defaultTitle') }}" />
+	<meta property="og:description" content="{{ Config::get('site.metaDesc') }}" />
+	<!--<meta name="twitter:card" content="summary"/> -->
+	<!--<meta name="twitter:title" content="{{ (!empty($title)) ? $title: Config::get('site.defaultTitle') }}" />-->
+	<!--<meta name="twitter:creator" content="@researchwap" />-->
+	<!--<meta name="twitter:description" content="{{ Config::get('site.metaDesc') }}" />-->
+    <!--<meta name="twitter:image" content="https://researchwap.com/assets/images/Project%20topics.webp" />-->
+
+
+	<!-- csrf -->
+	<meta name="csrf-token" content="{{ Session::token() }}">
+
+	<!-- favicons -->
+	<link rel="apple-touch-icon" sizes="57x57" type="image/png" href="{{ asset('assets/images/icons/apple-icon-57x57.png') }}">
+	<link rel="apple-touch-icon" sizes="60x60" type="image/png" href="{{ asset('assets/images/icons/apple-icon-60x60.png') }}">
+	<link rel="apple-touch-icon" sizes="72x72" type="image/png" href="{{ asset('assets/images/icons/apple-icon-72x72.png') }}">
+	<link rel="apple-touch-icon" sizes="76x76" type="image/png" href="{{ asset('assets/images/icons/apple-icon-76x76.png') }}">
+	<link rel="apple-touch-icon" sizes="114x114" type="image/png" href="{{ asset('assets/images/icons/apple-icon-114x114.png') }}">
+	<link rel="apple-touch-icon" sizes="120x120" type="image/png" href="{{ asset('assets/images/icons/apple-icon-120x120.png') }}">
+	<link rel="apple-touch-icon" sizes="144x144" type="image/png" href="{{ asset('assets/images/icons/apple-icon-144x144.png') }}">
+	<link rel="apple-touch-icon" sizes="152x152" type="image/png" href="{{ asset('assets/images/icons/apple-icon-152x152.png') }}">
+	<link rel="apple-touch-icon" sizes="180x180" type="image/png" href="{{ asset('assets/images/icons/apple-icon-180x180.png') }}">
+	<link rel="apple-touch-icon" sizes="192x192" type="image/png" href="{{ asset('assets/images/icons/apple-icon-192x192.png') }}">
+	<link rel="icon" sizes="192x192" type="image/png" href="{{ asset('assets/images/icons/android-icon-192x192.png') }}">
+	<link rel="icon" sizes="32x32" type="image/png" href="{{ asset('assets/images/icons/favicon-32x32.png') }}">
+	<link rel="icon" sizes="96x96" type="image/png" href="{{ asset('assets/images/icons/favicon-96x96.png') }}">
+	<link rel="icon" sizes="16x16" type="image/png" href="{{ asset('assets/images/icons/favicon-16x16.png') }}">
+	<link rel="manifest" href="{{ asset('assets/images/icons/manifest.json') }}">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="msapplication-TileImage" content="{{ asset('assets/images/icons/ms-icon-144x144.png') }}">
+	<meta name="theme-color" content="#ffffff">
+	
+	<script type="application/ld+json">{
+"@context": "https://schema.org/", "@type": "Article", "mainEntityOfPage": { "@type": "WebPage", "@id": "{{ Request::url() }}" }, "headline": "{{ (!empty($title)) ? $title: Config::get('site.defaultTitle') }}", "description": "{{ (!empty($meta)) ? $meta : Config::get('site.metaDesc')}}", "image": { "@type": "ImageObject", "url": "https://researchwap.com/assets/images/Research%20project%20topics.webp", "width": "696", "height": "521" }, "author": { "@type": "Organization", "name": "ResearchWap Inc." }, "publisher": { "@type": "Organization", "name": "ResearchWap Inc.", "logo": { "@type": "ImageObject", "url": "https://researchwap.com/assets/images/Project%20topics.webp", "width": "60", "height": "60" }
+  },
+  "datePublished": "2021-10-01T05:37:13-07:00",
+  "dateModified": "2021-10-01T05:37:13-07:00"
+}</script>
+
+ 
+   
+
+	@include('partials.header-scripts')
+	@yield('header-scripts')
+	@yield('page_css')
+	@yield('page_plugins_css')
+
+
+
+<style>
+ .gsc-adBlock, .gcsc-find-more-on-google-root, .gcsc-more-maybe-branding-root {
+           display: none !important;
+       }
+</style>
+
+<script async src="https://cse.google.com/cse.js?cx=ebdd9404063a3b0a3"></script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</head>
+
+
+
+<body id="mh-mobile" class="home page-template page-template-template-homepage page-template-template-homepage-php page page-id-6 wp-custom-logo mh-boxed-layout mh-right-sb mh-loop-layout4 mh-widget-layout8" itemscope="itemscope" itemtype="http://schema.org/WebPage">
+    
+	<div class="mh-container mh-container-outer">
+		@include('partials.header')
+		@include('index.partials.mobileNav')
+		@include('partials.n')
+
+		@yield('content')
+
+		@include('partials.footer')
+
+	</div>
+	@yield('footer-scripts')
+	 <div id="fb-root"></div> 
+<script async src='https://researchwap.com/assets/js/addlink.js' id='add_linkoncopy-js' defer></script>
+	 <script id='add_linkoncopy-js-extra'>
+/* <![CDATA[ */
+var astx_add_link_copied_text = {"readmore":"","addlinktosite":"","addsitename":"","breaks":"2","cleartext":"","reloption":"na","replaced_text":"All Rights Reserved | CALL or WHATSAPP our customer support on (+234) 0803-766-4978","target":"1","usesitenameaslink":"","usetitle":"1","sitename":"ResearchWap","siteurl":"https:\/\/researchwap.com","frontpage":"","pagetitle":"{{ Request::url() }}"};
+/* ]]> */
+</script>
+
+<!--<ins class="adsbygoogle" style="display:block; text-align:center;" data-ad-layout="in-article" data-ad-format="fluid" data-ad-client="ca-pub-5055690937774050" data-ad-slot="8304239462"></ins>-->
+<!--<script>-->
+<!--     (adsbygoogle = window.adsbygoogle || []).push({});-->
+<!--</script>-->
+
+
+	@yield('page_plugins')
+
+<div style="display:none;">
+<p><a title="Slot Pulsa" href="http://cc87912-drupal-7.tw1.ru/public/journals/slot-pulsa/" rel="dofollow">http://cc87912-drupal-7.tw1.ru/public/journals/slot-pulsa/</a></p>
+<p><a title="Slot Online" href="http://revistaeolo.fconvida.org/templates/images/slot-online/" rel="dofollow">http://revistaeolo.fconvida.org/templates/images/slot-online/</a></p>
+<p><a title="Slot Gacor 2022" href="http://revista.ispsn.org/ojs/slot-gacor/" rel="dofollow">http://revista.ispsn.org/ojs/slot-gacor/</a></p>
+<p><a title="Slot Deposit Pulsa" href="https://prosecutioncmms.kp.gov.pk/slot-pulsa/" rel="dofollow">https://prosecutioncmms.kp.gov.pk/slot-pulsa/</a></p>
+<p><a title="Slot Online Terpercaya" href="http://interactome.dfci.harvard.edu/fragdb/cgi-bin/judi-slot-online/" rel="dofollow">http://interactome.dfci.harvard.edu/fragdb/cgi-bin/judi-slot-online/</a></p>
+<p><a title="Rtp Slot" href="https://lolly.comc.co.il/" rel="dofollow">https://lolly.comc.co.il/</a></p>
+<p><a title="Slot Demo" href="https://wp01.fpe.zcu.cz/wp-includes/slot-demo/" rel="dofollow">https://wp01.fpe.zcu.cz/wp-includes/slot-demo/</a></p>
+<p><a title="Slot Gacor Hari Ini" href="https://ciae.icar.gov.in/vendor/-/slot-gacor/" rel="dofollow">https://ciae.icar.gov.in/vendor/-/slot-gacor/</a></p>
+<p><a title="situs slot gacor" href="https://trang.lib.ru.ac.th/slot-gacor/" rel="dofollow">https://trang.lib.ru.ac.th/slot-gacor/</a></p>
+<p><a title="jam rtp slot" href="https://careers.mahidol.ac.th/rtp-slot/" rel="dofollow">https://careers.mahidol.ac.th/rtp-slot/</a></p>
+<p><a title="Slot Pulsa Tanpa Potongan" href="https://www.jaotipe.hku.hk/slot/" rel="dofollow">https://www.jaotipe.hku.hk/slot/</a></p>
+<p><a title="Link Slot Gacor" href="https://novosite.fsh.edu.br/wp-includes/assets/slot-gacor/" rel="dofollow">https://novosite.fsh.edu.br/wp-includes/assets/slot-gacor/</a></p>
+<p><a title="bocoran slot gacor hari ini" href="https://nip.qau.edu.pk/wp/.tmb/slot-gacor/" rel="dofollow">https://nip.qau.edu.pk/wp/.tmb/slot-gacor/</a></p>
+<p><a title="slot via pulsa" href="http://adversus.org/revista/files/journals/slot-pulsa/" rel="dofollow">http://adversus.org/revista/files/journals/slot-pulsa/</a></p>
+<p><a title="slot gacor 4d" href="https://career.perumnas.co.id/assets/4d/" rel="dofollow">https://career.perumnas.co.id/assets/4d/</a></p>
+<p><a title="slot dana" href="https://rumah.perumnas.co.id/template/slot-dana/" rel="dofollow">https://rumah.perumnas.co.id/template/slot-dana/</a></p>
+<p><a title="slot gacor" href="https://perumnas.co.id/ninja-slot-88-gacor-4d/" rel="dofollow">https://perumnas.co.id/ninja-slot-88-gacor-4d/</a></p>
+</div>
+
+
+</body>
+
+</html>
